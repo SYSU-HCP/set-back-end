72e97b7cbea579fae654651dc0755bd11a26b561
haswelliris <haswelliris@live.com>
2018-07-10 00:48:16 +0800

    优化脚本交互

--- webhooks/autoIntegration.sh
+++ webhooks/autoIntegration.sh
@@ -1,12 +1,12 @@
 #! /bin/bash
 SITE_PATH='../set-back-end'
-
 cd $SITE_PATH
+echo "SYNC the Repository"
 git reset --hard origin/master
 git clean -f
 git pull
 git checkout master
-
+echo "DEPLOY the server"
 docker restart set-dl-server
 docker exec set-dl-server bash -c 'rm -rf /app/set-dl-server'
 docker cp . set-dl-server:/app/set-dl-server
--- webhooks/continuousIntegration.js
+++ webhooks/continuousIntegration.js
@@ -1,35 +1,39 @@
-var http = require('http')
-var spawn = require('child_process').spawn
-var createHandler = require('github-webhook-handler')
-let config = require('./config')
+var http = require('http');
+var spawn = require('child_process').spawn;
+var createHandler = require('github-webhook-handler');
+const config = require('./config');
 // 下面填写的 myscrect 跟github webhooks配置一样；
 // path是我们访问的路径
 var handler = createHandler(config);
 
 http.createServer(function (req, res) {
   handler(req, res, function (err) {
-    res.statusCode = 404
-    res.end('no such location: ' + err.message)
+    res.statusCode = 404;
+    res.end('no such location: ' + err.message);
   })
-}).listen(8989)
+}).listen(8989);
+
+console.log('start listen 8989 ...');
 
 handler.on('error', function (err) {
-  console.error('Error:', err.message)
+  console.error('Error:', err.message);
 })
 
 // 监听到push事件的时候执行我们的自动化脚本
 handler.on('push', function (event) {
   console.log('Received a push event for %s to %s',
     event.payload.repository.name,
-    event.payload.ref)
-  runCommand('sh', ['./autoIntegration.sh'], function (txt) {
-    console.log(txt)
-  })
+    event.payload.ref);
+  for (var i in config.scripts) {
+    runCommand('sh', ['./autoIntegration.sh'], function (txt) {
+      console.log(txt);
+    })
+  }
 })
 
 function runCommand(cmd, args, callback) {
-  var child = spawn(cmd, args)
-  var response = ''
-  child.stdout.on('data', function (buffer) { response += buffer.toString() })
-  child.stdout.on('end', function () { callback(response) })
+  var child = spawn(cmd, args);
+  var response = '';
+  child.stdout.on('data', function (buffer) { response += buffer.toString() });
+  child.stdout.on('end', function () { callback(response) });
 }
